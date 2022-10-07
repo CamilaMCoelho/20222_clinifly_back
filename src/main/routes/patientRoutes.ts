1b42fff432695809e050707ed40c161912ee5000
Italo Alves <italo_silva007@hotmail.com>
2022-10-07 20:01:09 -0300

    fix error read routes

--- src/main/routes/patientRoutes.ts
+++ src/main/routes/patientRoutes.ts
@@ -3,7 +3,8 @@ import { adaptRoute } from '../adapters/expressRouteAdapter'
 import { makeCreatePatientController } from '../factories/controllers/createPatientController/createPatientControllerFactory'
 import { makeAuthPatientController } from '../factories/controllers/authPatientController/authPatientControllerFactory'
 
-export default (router: Router): void => {
+export default (): void => {
+  const router = Router()
   router.post('/patients', adaptRoute(makeCreatePatientController()))
   router.post('/auth', adaptRoute(makeAuthPatientController()))
 }
